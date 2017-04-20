6381cebeb90c5dd2a06888e08851f369104f48e0
Landon West <landonwest5@gmail.com>
2017-04-20 16:27:44 -0700

    add autoprefixer and normalizer & bundle; update mobile styles; add scroll-up function

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,4 +4,8 @@ $( document ).on('turbolinks:load', function() {
     $('html, body').animate({ scrollTop: $('.content-background').offset().top }, 1000);
   });
 
+  $('#scroll-up').on('click', function() {
+    $('html, body').animate({ scrollTop: $('.headline-background').offset().top }, 1000);
+  });
+
 });
