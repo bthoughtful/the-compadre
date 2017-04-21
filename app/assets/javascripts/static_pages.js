ea754be8498efc75777e931d4b58efffdf085abf
Landon West <landonwest5@gmail.com>
2017-04-21 14:56:39 -0700

    add snapScroll function; update styles

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,11 +1,35 @@
+function snapScroll() {
+  var delay = false;
+
+  $(document).on('mousewheel DOMMouseScroll', function(event) {
+    event.preventDefault();
+    if(delay) return;
+
+    delay = true;
+    setTimeout(function(){ delay = false }, 200)
+
+    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+    var topSection = document.querySelector('#topSection');
+    var bottomSection = document.querySelector('#bottomSection');
+
+    if (wd > 0) {
+      $('html,body').animate({ scrollTop: topSection.offsetTop });
+    } else {
+      $('html,body').animate({ scrollTop: bottomSection.offsetTop });
+    }
+  });
+}
+
 $( document ).on('turbolinks:load', function() {
 
+  snapScroll();
+
   $('#scroll-down').on('click', function() {
-    $('html, body').animate({ scrollTop: $('.content-background').offset().top }, 1000);
+    $('html, body').animate({ scrollTop: $('.content-background').offset().top }, 500);
   });
 
   $('#scroll-up').on('click', function() {
-    $('html, body').animate({ scrollTop: $('.headline-background').offset().top }, 1000);
+    $('html, body').animate({ scrollTop: $('.headline-background').offset().top }, 500);
   });
 
 });
