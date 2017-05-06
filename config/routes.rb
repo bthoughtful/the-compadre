981af9f954c17cb0062cdc0200bd334a06498c1e
Landon West <landonwest5@gmail.com>
2017-05-06 11:38:39 -0700

    create articles controller w/ index & show actions; fill-in index/show views; create article routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/show'
+
+  get 'index/show'
+
   root to: 'static_pages#index'
   devise_for :users
+  resources :articles, only: [:index, :show]
   resource :newsletters, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
