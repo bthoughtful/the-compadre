e5eaa34725eaa90185312c73d4f93bb1c693e503
Landon West <landonwest5@gmail.com>
2017-05-17 14:22:59 -0700

    config action_mailer_assets host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.asset_host = ENV['DOMAIN_NAME']
 
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
