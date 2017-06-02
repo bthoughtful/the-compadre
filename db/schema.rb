cfd484f77d6d6e02eefa5c12bc0684e51d252b61
Landon West <landonwest5@gmail.com>
2017-06-02 14:42:05 -0700

    add published_at column to articles table; create parse_date helper; updates views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509233902) do
+ActiveRecord::Schema.define(version: 20170602200708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20170509233902) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "intro"
+    t.string "published_at"
     t.index ["author"], name: "index_articles_on_author"
     t.index ["created_at"], name: "index_articles_on_created_at"
+    t.index ["published_at"], name: "index_articles_on_published_at"
   end
 
   create_table "users", force: :cascade do |t|
